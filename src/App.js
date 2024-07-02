a05f8e9c3c5f84b44dffbfa0f8594de830b5e94f
pnzaou <perrinemmanuelnzaou@gmail.com>
2024-07-02 13:56:47 +0000

    Ajout des commentaires

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import TaskList from './components/TaskList';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]); //J'initialise l'état de la liste de tache à afficher avec un tableau vide
   const [currentTask, setCurrentTask] = useState(null);
 
+  //Dans ce useEffect j'utilise la méthode getItem de localstorage pour récupérer les taches stockées et les transforme en objet 
   useEffect(() => {
     const storedTasks = JSON.parse(localStorage.getItem('tasks'));
     if (storedTasks) {
@@ -14,16 +15,17 @@ function App() {
     }
   }, []);
 
+  //Dans ce useEffect j'utilise la méthode setItem pour enregister les taches sous forme de chaîne de caractères JSON
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
 
   const handleSaveTask = (task) => {
     if (task.id) {
-      setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+      setTasks(tasks.map((t) => (t.id === task.id ? task : t))); //Ici je vérifie si la tache existe déjà dans un cas de modification
     } else {
-      task.id = Date.now();
-      setTasks([...tasks, task]);
+      task.id = Date.now(); //Ici j'attribut la date de creation comme id
+      setTasks([...tasks, task]); //j'utilise l'operateur spread pour faire une copie du tableau de tache dans laquelle j'ajoute la nouvelle tache
     }
     setCurrentTask(null);
   };
@@ -34,13 +36,13 @@ function App() {
 
   const handleDeleteTask = (task) => {
     if (window.confirm('Voulez-vous vraiment supprimer cette tâche?')) {
-      setTasks(tasks.filter((t) => t.id !== task.id));
+      setTasks(tasks.filter((t) => t.id !== task.id)); //ici je supprime une tache en renvoyant un nouveau tableau avec les taches dont l'id est different de celui de la tache passé en parametre
     }
   };
 
   const handleToggleTask = (task) => {
     setTasks(
-      tasks.map((t) => (t.id === task.id ? { ...t, completed: !t.completed } : t))
+      tasks.map((t) => (t.id === task.id ? { ...t, completed: !t.completed } : t)) //ici dans le cas ou la tache est trouvée je modifie son attribut completed
     );
   };
 
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 const TaskForm = ({ task, onSave }) => {
-    
+
+    //Ici j'initialise la valeur des input nom et description
     const [name, setName] = useState(task ? task.name : '');
     const [description, setDescription] = useState(task ? task.description : '');
 
+    //handleSubmit est la fonction que j'appelle lors de la soumition du formulaire
     const handleSubmit = (e) => {
         e.preventDefault();
         if (name && description) {
@@ -23,18 +25,8 @@ const TaskForm = ({ task, onSave }) => {
 
     return (
         <form onSubmit={handleSubmit}>
-        <input
-            type="text"
-            placeholder="Nom de la tâche"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-        />
-        <input
-            type="text"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-        />
+        <input type="text" placeholder="Nom de la tâche" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
         <button type="submit">Enregistrer</button>
         </form>
     );
